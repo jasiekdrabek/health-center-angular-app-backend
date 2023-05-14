8fa83c67ba9473eacc9018d87e36f329cb53f483
Jan Drabek <jasiekdrabek@gmail.com>
2023-05-14 16:13:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const port = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+  );
+  next();
+});
+
 require("./database")();
 
 app.use(bodyParser.json());
